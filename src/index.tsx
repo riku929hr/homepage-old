679d8845f2fb86e7632ac3944bc8b5f04eb82f54
riku929hr <sugarhorn0929@gmail.com>
2021-06-26 18:32:29 +0900

    add semantic-ui

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
