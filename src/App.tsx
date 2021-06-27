a1d21418320c74e0519c07c930bd69ef1927b17b
riku929hr <sugarhorn0929@gmail.com>
2021-06-27 23:17:40 +0900

    [add] SNS Icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { FC } from 'react';
-import { Header, Container } from 'semantic-ui-react';
+import { Header, Container, Icon, Image } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
 import './App.css';
+import hatenablogLogo from './hatena-blog-logo-s.png';
+import qiitaLogo from './qiita.png';
 
 const style = {
   h1: {
@@ -40,10 +42,31 @@ const App: FC = () => (
       <Header as="h3">開発環境</Header>
       <Header as="h3">資格など</Header>
     </Container>
-    <Container style={style.container} text>
+    <Container style={style.container} textAlign="center">
       <Header as="h2" textAlign="center">
         SNS
       </Header>
+      <a href="https://twitter.com/riku929hr">
+        <Icon name="twitter" size="big" link />
+      </a>
+      <a href="https://github.com/riku929hr">
+        <Icon name="github" size="big" link color="black" />
+      </a>
+
+      <Image
+        src={qiitaLogo}
+        href="https://qiita.com/riku929hr"
+        link
+        avatar
+        inline
+      />
+      <Image
+        src={hatenablogLogo}
+        href="https://riku929hr.hatenablog.com"
+        avatar
+        link
+        inline
+      />
     </Container>
   </>
 );
