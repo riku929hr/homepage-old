ab5b89db09316359e22791682f61617487196e84
riku929hr <sugarhorn0929@gmail.com>
2021-12-30 17:23:29 +0900

    [add] note link and icon

--- src/data/sns.ts
+++ src/data/sns.ts
@@ -2,6 +2,7 @@ import twitterIcon from './icons/twitter.png';
 import githubIcon from './icons/github.png';
 import qiitaIcon from './icons/qiita.png';
 import hatenaIcon from './icons/hatena.png';
+import noteIcon from './icons/note.png';
 
 export type Sns = {
   id: number;
@@ -38,6 +39,12 @@ export const snsData: Sns[] = [
   },
   {
     id: 5,
+    name: 'note',
+    link: 'https://note.com/riku929hr',
+    iconSrc: noteIcon,
+  },
+  {
+    id: 6,
     name: 'YOUTRUST',
     link: 'https://youtrust.jp/users/riku929hr',
     description: '詳しい職務経歴はこちらに載せています。',
